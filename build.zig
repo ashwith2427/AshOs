cc0438f44b8200dc909fb010e6c146b990812b9d
Vaddepally Ashwith Chandra <ashwith@Terminator.>
2025-02-01 20:03:28 +0530

    Elf to image

--- build.zig
+++ build.zig
@@ -1,10 +1,16 @@
 const std = @import("std");
 pub fn build(b: *std.Build) void {
-    const target = b.resolveTargetQuery(.{ .cpu_arch = .aarch64, .os_tag = .freestanding });
+    const target = b.resolveTargetQuery(
+        .{
+            .cpu_arch = .aarch64,
+            .os_tag = .freestanding,
+            .ofmt = .elf,
+        },
+    );
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "OS",
+        .name = "OS.elf",
         .root_source_file = b.path("src/kernel.zig"),
         .target = target,
         .optimize = optimize,
